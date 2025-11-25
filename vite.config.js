0f488c037ae8c91ac9e40f8d62508a3d619a4044
Aakash Verma <akashverma611081@gmail.com>
2025-11-25 14:04:20 +0530

    changing vite.config file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import tailwindcss from '@tailwindcss/vite';  // New Vite plugin
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],  // Add the plugin
+    server: {
+    proxy: {
+      "/api": {
+        target: "http://15.206.128.54:4000",
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 });
